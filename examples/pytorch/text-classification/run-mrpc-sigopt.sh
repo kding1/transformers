6c2166ea128404cb05798aba0f21918c5e739e20
Ding, Ke <ke.ding@intel.com>
2021-11-19 19:44:15 -0800

    add sigopt hpo tuning for glue.
    Signed-off-by: Ding, Ke <ke.ding@intel.com>

---
+++ examples/pytorch/text-classification/run-mrpc-sigopt.sh
@@ -0,0 +1,3 @@
+
+time(python run_glue.py --model_name_or_path bert-base-cased --task_name mrpc --do_train --do_eval --use_sigopt_hpo True --max_seq_length 128 --output_dir /tmp/mrpc/ --overwrite_output_dir True)
+
